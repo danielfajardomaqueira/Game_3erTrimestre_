9af1a89382e31706147af6d616ac0944a92e9660
danielfajardomaqueira <114388728+danielfajardomaqueira@users.noreply.github.com>
2023-05-30 02:54:43 +0200

    V10_

--- Assets/Scripts/countCollectable.cs
+++ Assets/Scripts/countCollectable.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class countCollectable : MonoBehaviour
 {
 
     public int collectableOne = 0;
+    private int maxCollectableOne = 17;
     public int collectableTwo = 0;
+
+    public GameObject redInvisibleWall;
+    public TMP_Text coinText;
+    //public static CoinCounter instance;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,6 +20,8 @@ public class countCollectable : MonoBehaviour
         {
             Destroy(other.gameObject);
             collectableOne++;
+            coinText.text = "COINS:" + collectableOne.ToString();
+            
         }
 
       if (other.CompareTag("collectableTwo"))
@@ -24,4 +32,22 @@ public class countCollectable : MonoBehaviour
     }
 
 
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (collectableOne == 17)
+        {
+            Destroy(redInvisibleWall);
+        }
+    }
+
+    private void Awake()
+    {
+       // instance = this;
+    }
 }
